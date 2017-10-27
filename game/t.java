dd55df5ac4310ef9b4ee827fae7e68602c5edfb8
Andre Wang <andre.wang0001@gmail.com>
2017-10-27 15:25:28 +0800

    test file

---
+++ game/t.java
@@ -0,0 +1,10 @@
+/**
+	Test if the file can compile.
+*/
+public class t {
+    public static void main(String[] args) {
+        MossSideWhist msw = new MossSideWhist(new Agent21725083(), new RandomAgent(), new Agent21714084());
+        msw.playGame(1, System.out);
+	System.out.println(msw.getScores());
+    }
+}
